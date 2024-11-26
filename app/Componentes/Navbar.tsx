ae8ef3974ec589a97892e0448d3d5aa6bfe341ee
MUHAMMADFARHANHANIF <farhansmit0318@gmail.com>
2024-11-26 23:51:49 +0500

    all ok

--- app/Componentes/Navbar.tsx
+++ app/Componentes/Navbar.tsx
@@ -6,17 +6,17 @@ const Navbar = () => {
   return (
     <>
     
-    <div className='flex w-full    justify-between flex-wrap'>
+    <div className='flex w-full border-b-2   justify-between flex-wrap'>
         
         
         <div className='flex  w-full lg:w-72 md:justify-around bg-cover '>
-            <Image width={200} height={200} src={logo} className=' m-4' alt='ummat'/>
+            <Image width={200} height={200} src={logo} className=' m-2' alt='ummat'/>
             {/* <h2 className='text-xl'>Ummat Electronics</h2> */}
             <div className='lg:hidden  ' >Menu</div>
         </div>
 
 
-        <div className='gap-10 m-auto text-lg bg-orange-300 md:flex w-96 flex-row  justify-between items-center hidden '>
+        <div className='gap-10 m-auto text-xl  lg:flex w-80 flex-row  justify-between items-center hidden '>
             <a href="" className=' hover:underline'>Home</a>
             <a href="" className=''>About</a>
             <a href="" className=''>Blog</a>
@@ -33,15 +33,17 @@ const Navbar = () => {
 
 
 
-        <div className='flex md:w-60 sm:w-80 lg:w-80 bg-yellow-300 justify-between items-center gap-3'>
+        <div className='flex p-4 sm:w-96 w-full bg--300 justify-between items-center '>
             <div className='text-center md:text-left'>
-                <h2>Call Now</h2>
+                <h2>Call Now </h2>
+                <h2>+92 344 259 8379</h2>
             </div>
             <div className='text-center md:text-left'>
                 Add Cart
             </div>
             <div className='text-center md:text-left'>
-                <button>Login/Register</button>
+                <button className='text-center md:text-left'>Log in <br />
+                    Register</button>
             </div>
         </div>
 
