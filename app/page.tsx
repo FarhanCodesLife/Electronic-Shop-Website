865632e81b4b0cc0b44a76bed575e3221065449a
farhancodeslife <farhansmit0318@gmail.com>
2025-01-07 08:59:03 -0800

    all ok

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,10 @@
 import React from 'react'
-import Home from './Pages/Home/page'
 
 // import 'bootstrap/dist/css/bootstrap.min.css';
 
 const page = () => {
   return (
     <>
-    <Home/>
     </>
   )
 }
